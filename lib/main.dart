511925042bc3ba92611ee26a15dfee2e7fe38fb1
clemenex <clemenexcortex@gmail.com>
2025-05-18 21:57:18 +0800

    feat: add barebone main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cheez_quiz_app/pages/home_page.dart';
 import 'package:cheez_quiz_app/pages/login_student.dart';
 import 'package:cheez_quiz_app/pages/login_teacher.dart';
+import 'package:cheez_quiz_app/pages/main_teacher.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,11 +16,12 @@ class CheezQuizApp extends StatelessWidget {
     return MaterialApp(
       title: 'Cheez Quiz',
       debugShowCheckedModeBanner: false,
-      initialRoute: '/login_teacher',
+      initialRoute: '/home',
       routes: {
         '/home': (context) => const HomePage(),
         '/login_teacher': (context) => const TeacherLoginPage(),
         '/login_student': (context) => const StudentLoginPage(),
+        '/main_teacher': (context) => const TeacherMainPage(),
       }
     );
   }
